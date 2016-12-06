7586372c45cc7cb5fb8d44552108348a74462a88
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-12-06 19:31:27 +0100

    added method sort by first letter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   def home
     @result = find_gender
+    @result_first_letter = first_letter
   end
 
   def find_gender
     @masculine = Name.where(gender: "masculine")
+    @feminine = Name.where(gender: "feminine")
+  end
+
+  def first_letter
+    name = Name.downcase
+    @result_first_letter = Name.where(name.initial == "a")
   end
 
 
