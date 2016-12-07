99ab97cf2f8a56e91a24138e58b89ade9ad3be52
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-12-07 11:07:01 +0100

    starting off

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @result = find_gender
-    @result_first_letter = first_letter
+    # @result_first_letter = first_letter
   end
 
   def find_gender
@@ -9,10 +9,10 @@ class PagesController < ApplicationController
     @feminine = Name.where(gender: "feminine")
   end
 
-  def first_letter
-    name = Name.downcase
-    @result_first_letter = Name.where(name.initial == "a")
-  end
+  # def first_letter
+  #   name = Name.downcase
+  #   @result_first_letter = Name.where(name.initial == "a")
+  # end
 
 
 
