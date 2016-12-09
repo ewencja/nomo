1188d833a4e61e05e4f2d33711b29a1f907a9165
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-12-09 17:48:23 +0100

    lots of stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,65 +2,69 @@ require 'open-uri'
 require 'nokogiri'
 
 class PagesController < ApplicationController
-   def gender
+  def gender
     @gender = ["masculine", "feminine", "unisex"]
   end
 
-  def soundex
-  end
+  # def soundex
+  # end
 
   def home
     @gender = ["masculine", "feminine", "unisex"]
-    get_names_origin
+    # get_names_origin
     @length = ["short", "medium", "long"]
     # @result = find_gender
     # @result_first_letter = first_letter
   end
-
-  def get_names_origin
-    @origin_names = []
-    @origin = Origin.all
-    @origin.each do |origin|
-      @origin_names << origin.origin
-    end
-  end
+  #
+  # def get_names_origin
+  #   @origin_names = []
+  #   @origin = Origin.all
+  #   @origin.each do |origin|
+  #     @origin_names << origin.origin
+  #   end
+  # end
 
 
 
   def results
-    if params[:gender].present? || params[:origin].present? || params[:length].present?
-      @names = Name
-        .joins(:origin)
-        .where(
-          'names.gender' => params[:gender],
-          'names.length' => params[:length],
-          'origins.origin' => [params[:origin0], params[:origin1]]);
-    else
-      @names = Name.all
-    end
-
-    @sample = Name.where(:gender => params[:gender]).sample(5)
+#     if session[:gender].present? || params[:origin0].present? || params[:origin1].present? || params[:length].present?
+      # @names = Name
+      #   .joins(:origin)
+      #   .where(
+      #     'names.gender' => session[:gender],
+      #     'names.length' => params[:length],
+      #     'origins.origin' => [params[:origin0], params[:origin1]]);
+      @names = Name.find_by_sql("SELECT b.name
+          FROM names n
+          JOIN names_origins n_o
+          	ON n_o.name_id = n.id
+          JOIN origins o
+          	ON n_o.origin_id = o.id
+          JOIN names b
+          	ON left(n.name, 1) = left(b.name, 1) and right(n.name, 1) = right(b.name, 1) and length(n.metaphone) = length(b.metaphone) and n.gender = b.gender
+          WHERE (o.origin = '#{params[:origin0]}' or o.origin = '#{params[:origin1]}') and n.gender = '#{session[:gender]}'").sample(7)
+    # else
+    #   @names = Name.all
+    # end
 
   end
 
   def reset_session
     session.clear
     redirect_to root_path
+
   end
 
-  def search_for_name
-
-    @sample = Name.where(:gender => params[:gender]).sample(6)
-
-
-    a = "Anna" #this name has a metaphone index
-    b = "Alicia" #this name has a metaphone index
-
-    @soundex_names = Name.find_by_sql("SELECT b.name, b.metaphone from NAMES a
-      JOIN NAMES b
-      ON left(a.name, 1) = left(b.name, 1) and right(a.name, 1) = right(b.name, 1) and length(a.metaphone) = length(b.metaphone)
-      WHERE a.name = '#{a}' or a.name = '#{b}'")
-  end
+  # def search_for_name
+  #   a = "Anna" #this name has a metaphone index
+  #   b = "Alicia" #this name has a metaphone index
+  #
+  #   @soundex_names = Name.find_by_sql("SELECT b.name, b.metaphone from NAMES a
+  #     JOIN NAMES b
+  #     ON left(a.name, 1) = left(b.name, 1) and right(a.name, 1) = right(b.name, 1) and length(a.metaphone) = length(b.metaphone)
+  #     WHERE a.name = '#{a}' or a.name = '#{b}'")
+  # end
 
   #
   # def search_for_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209120308) do
+ActiveRecord::Schema.define(version: 20161209162121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 20161209120308) do
     t.string   "origin",     null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+    t.index ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
   end
 
   add_foreign_key "frequencies", "names"
