f2021acb34cb3cd6a282907c81ca280a1790cef8
Ewa Treitz <ewa.grzechnik@gmail.com>
2016-12-13 17:00:13 +0100

    soundex implemented on names info page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,27 +6,6 @@ class PagesController < ApplicationController
     @gender = ["masculine", "feminine", "unisex"]
   end
 
-  # def home
-  #   @gender = ["masculine", "feminine", "unisex"]
-  #   @length = ["short", "medium", "long"]
-  # end
-  #
-  # def results
-  #   @names = Name.find_by_sql("SELECT b.name, b.gender
-  #       FROM names n
-  #       JOIN names_origins n_o
-  #       	ON n_o.name_id = n.id
-  #       JOIN origins o
-  #       	ON n_o.origin_id = o.id
-  #       JOIN names b
-  #       	ON n.metaphone = b.metaphone and n.gender = b.gender
-  #       WHERE (o.origin = '#{params[:origin0]}' or o.origin = '#{params[:origin1]}') and n.gender = '#{session[:gender]}' and n.name = '#{cookies[:soundex][0]}'").sample(7)
-  # end
-  #
-  # def reset_session
-  #   session.clear
-  #   redirect_to root_path
-  # end
 
   def frequency_by_name
     @output = ActiveRecord::Base.connection.execute("
@@ -37,5 +16,4 @@ class PagesController < ApplicationController
       WHERE LOWER(n.name) = LOWER('#{params[:name]}')")
     render json: @output
   end
-
 end
